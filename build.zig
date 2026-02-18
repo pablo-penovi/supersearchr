7213856d8ca387bb1df78e6f298701801dbd55fb
Pablo Penovi <pablopenovi@gmail.com>
2026-02-18 15:39:35 -0300

    Fix test runner to include config tests
    - Add explicit test step for config.zig module in build.zig
    - Tests now run via 'zig build test'

--- build.zig
+++ build.zig
@@ -125,6 +125,16 @@ pub fn build(b: *std.Build) void {
     // A run step that will run the test executable.
     const run_mod_tests = b.addRunArtifact(mod_tests);
 
+    // Test for config module
+    const config_tests = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/config.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+    const run_config_tests = b.addRunArtifact(config_tests);
+
     // Creates an executable that will run `test` blocks from the executable's
     // root module. Note that test executables only test one module at a time,
     // hence why we have to create two separate ones.
@@ -136,11 +146,12 @@ pub fn build(b: *std.Build) void {
     const run_exe_tests = b.addRunArtifact(exe_tests);
 
     // A top level step for running all tests. dependOn can be called multiple
-    // times and since the two run steps do not depend on one another, this will
+    // times and since the two run steps does not depend on one another, this will
     // make the two of them run in parallel.
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_mod_tests.step);
     test_step.dependOn(&run_exe_tests.step);
+    test_step.dependOn(&run_config_tests.step);
 
     // Just like flags, top level steps are also listed in the `--help` menu.
     //
